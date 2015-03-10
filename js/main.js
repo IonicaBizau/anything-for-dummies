e7b87ada75853641d60983cb02512d2822d6e6c5
Ionică Bizău <bizauionica@yahoo.com>
2015-03-10 16:00:12 +0200

    Share buttons, create, footer

--- js/main.js
+++ js/main.js
@@ -7,9 +7,36 @@
           ;
 
         titleConf.textContent = title;
-        urlForDummies.href = src;
+        urlForDummies.href = decodeURIComponent(src);
     }
 
-    window.addEventListener("load", loadText);
-    window.addEventListener("hashchange", loadText);
+    function updateShareLinks() {
+        var link = Url.getLocation();
+        document.querySelector(".share.facebook").href = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(link);
+        document.querySelector(".share.twitter").href = "http://twitter.com/intent/tweet?text=" + encodeURIComponent(Url.queryString("title") + " for dummies #FORDUMMIES") + "&url=" + encodeURIComponent(link);
+        document.querySelector(".share.gplus").href = "https://plus.google.com/share?url=" + encodeURIComponent(link);
+    }
+
+    function checkHash() {
+        var hash = location.hash.substring(1)
+          , title = null
+          , link = null
+          ;
+
+        if (hash === "create") {
+            Url.updateSearchParam("title", prompt("Title"));
+            Url.updateSearchParam("link", prompt("link"));
+        }
+
+        location.hash = "";
+        loadText();
+        updateShareLinks();
+    }
+
+    window.addEventListener("load", function () {
+       loadText();
+       checkHash();
+    });
+
+    window.addEventListener("hashchange", checkHash);
 })();
